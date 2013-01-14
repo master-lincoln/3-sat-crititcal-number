5fb1e1b9529fcc9b82db303978ef9b6148a7f8aa
neogucky <neogucky@gmail.com>
2013-01-14 16:50:30 +0100

    Started creation of randomSATSolver
    With my iPad 

--- src/de/unihl/algo/three_sat_crititcal_number/RandomSATSolver.java
+++ src/de/unihl/algo/three_sat_crititcal_number/RandomSATSolver.java
@@ -2,4 +2,17 @@ package de.unihl.algo.three_sat_crititcal_number;
 
 public class RandomSATSolver {
 
+Private CNFFormula formula;
+private int n;
+
+Public void RandomSatSolver(CNFFormula _formula, int _n){
+Formula = _formula;
+n = _n;
+//set new values for all variables in the formulas setting
+Vector<Boolean> setting = new Vector<Boolean>();
+Random random =new Random();
+for (int i = 0;i < n;i++){
+setting.set(i,random.nextBoolean());
+}
+formula.setValues(setting);
 }

783a32fead907c34e0a62895376ba4d49a4e37fe
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2013-01-20 11:34:56 +0100

    final stage reached, included CSV generation for task b, jar file with dependencies

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 
 You need [Maven 2.2.x](http://maven.apache.org/download.cgi) to build the project.
 I also recommend using the [Maven2eclipse Plugin](http://eclipse.org/m2e/download/).
-Use the command ``mvn package`` to create a .jar file.
+Use the command ``mvn package`` to create a .jar file or ``mvn assembly:single`` to create a jar including all dependencies.
 This also downloads the needed dependencies at the first time.
 
 
 To start the program outside an IDE use
- ``java -jar target/3-sat-crititcal-number-0.0.1-SNAPSHOT.jar``
+ ``java -jar target/3-sat-crititcal-number-0.0.1-SNAPSHOT-jar-with-dependencies.jar ``
--- src/main/java/de/unihl/algo/three_sat_crititcal_number/Main.java
+++ src/main/java/de/unihl/algo/three_sat_crititcal_number/Main.java
@@ -1,5 +1,7 @@
 package de.unihl.algo.three_sat_crititcal_number;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -8,6 +10,8 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.perf4j.StopWatch;
 import org.perf4j.log4j.Log4JStopWatch;
+
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class Main {
 	
@@ -24,48 +28,81 @@ public class Main {
 		int n = 9;
 		int bestM  = 40;
 		
-		// TODO für beide Verteilungen i
-		while ( t_avg < 3000 ) {
-			n += 1;
-			// empirical probability for each m
-			Map<Integer,Double> sigmas = new TreeMap<Integer, Double>();
-			t_avg = 0;
+		for (int i=1; i<3; i++) {
+			log.info("Starting tests for distribution i="+i);
 			
-			// values obtained by testing
-			int start = (int) (4*n);
-			int end = (int) (5*n);
+			while ( t_avg < 3000 ) {
+				n += 1;
+				// empirical probability for each m
+				Map<Integer,Double> sigmas = new TreeMap<Integer, Double>();
+				t_avg = 0;
+				
+				// values obtained by testing
+				int start = (int) (4*n);
+				int end = (int) (5*n);
+				
+				for (int m=start; m<end; m++) {
+					StopWatch avgStopWatch = new Log4JStopWatch("full.n"+n+".m"+m);
+					int satisfieable = 0;
+					
+					// 100 times
+					for (int j=0;j<100;j++) {
+						
+						CNFFormula formula = new CNFFormula(k);
+						formula.randomizeFormula(1, n, m);
+						satisfieable += RandomSATSolver.isSatisfiable(formula,n) ? 1 : 0;
+						
+					}
+					
+					sigmas.put(m, (double)satisfieable/100);
+					
+					avgStopWatch.stop();
+					long this_avg = avgStopWatch.getElapsedTime()/100;
+					t_avg = t_avg < this_avg ? this_avg : t_avg;
+				}
+				
+				
+				log.debug(sigmas);
+				bestM = getBestM(sigmas, n);
+				
+			}
+			
+			// Task B
+			// n = n_max at this point with corresponding bestM
+			
+			Map<Integer,Double> sigmas = new TreeMap<Integer, Double>();
+			int start = (int) Math.floor(0.9*bestM);
+			int end = (int) Math.ceil(1.1*bestM);
 			
 			for (int m=start; m<end; m++) {
-				StopWatch avgStopWatch = new Log4JStopWatch("full.n"+n+".m"+m);
 				int satisfieable = 0;
 				
 				// 100 times
-				for (int i=0;i<100;i++) {
-					StopWatch stopWatch = new Log4JStopWatch("n"+n+".m"+m);
+				for (int j=0;j<100;j++) {
 					
 					CNFFormula formula = new CNFFormula(k);
 					formula.randomizeFormula(1, n, m);
 					satisfieable += RandomSATSolver.isSatisfiable(formula,n) ? 1 : 0;
 					
-					stopWatch.stop();
 				}
-				
 				sigmas.put(m, (double)satisfieable/100);
-				
-				avgStopWatch.stop();
-				long this_avg = avgStopWatch.getElapsedTime()/100;
-				t_avg = t_avg < this_avg ? this_avg : t_avg;
 			}
-			
-			
-			log.debug(sigmas);
-			bestM = getBestM(sigmas, n);
-			
+			log.info("Task B solution:");
+			log.info(sigmas);
+			createCSV("Sigmas for i="+i+",nmax="+n+" and m="+bestM, sigmas);
 		}
-		// n = n_max at this point
 		
 	}
 	
+	private static void createCSV(String filename, Map<Integer, Double> sigmas) throws IOException {
+		CSVWriter writer = new CSVWriter(new FileWriter(filename+".csv"), '\t');
+		for ( int m : sigmas.keySet() ) {
+			String[] entries = { String.valueOf(m), String.valueOf(sigmas.get(m)) };
+			writer.writeNext(entries);
+		}
+		writer.close();
+	}
+
 	private static int getBestM(Map<Integer, Double> sigmas, int n) {
 		int bestM = -1;
 		double bestValue = -1;
